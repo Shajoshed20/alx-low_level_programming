644816f507711c0260a889afe2fb2ff003823a22
Shajoshed20 <ugobelle@gmail.com>
2023-02-16 09:54:07 -0800

    Using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/*string initialisation*/
-	char myPut[] = "Programming is like building a multilingual puzzle";
+	char myPut[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myPut); /*writing the string as print out*/
 
