ed330342b0bdfdb4c9f9f2223aff29d7219b760c
Shajoshed20 <ugobelle@gmail.com>
2023-02-16 09:08:15 -0800

    using puts funtion

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*A program that uses puts function*/
+int main(void) 
+{
+	/*string initialisation*/
+	char myPut[] = "Programming is like building a multilingual puzzle";
+
+	puts(myPut); /*writing the string as print out*/
+
+	return (0);
+}
