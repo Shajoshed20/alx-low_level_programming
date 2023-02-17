bf37c846a900cc5bd9003828ab48c1a48ebcaa1f
Shajoshed20 <ugobelle@gmail.com>
2023-02-17 07:48:27 -0800

    print numbers to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*single digits 0-9*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+}
