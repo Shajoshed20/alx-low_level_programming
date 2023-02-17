9e814651eb13499623c6c85f8dedd071738813e6
Shajoshed20 <ugobelle@gmail.com>
2023-02-17 07:10:32 -0800

    Print alphebets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*for loop used in lowercase*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCase > 'z'; lowerCase++)
+	{
+		putchar(lowerCase);
+	}
+	putchar('\n');
+	return (0);
+
+}
