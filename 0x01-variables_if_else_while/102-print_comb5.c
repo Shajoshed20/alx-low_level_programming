56eb7923e5c9988b85eed76ec5662ee72ebad170
Shajoshed20 <ugobelle@gmail.com>
2023-02-17 10:01:44 -0800

    Print 2 pairs of digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*Two pairs of digits*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar(a + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar(b + '0');
+
+			if (a == 98 && b == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
