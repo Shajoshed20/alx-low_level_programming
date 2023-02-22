448af3f9307696a847074f49065287d084fe374d
Shajoshed20 <ugobelle@gmail.com>
2023-02-21 21:19:16 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the summ of all multiple of 3 or 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			sum += 1;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
