dbc3ddcc9bf6bb5113a1af9b0d79b7faac29dd96
Shajoshed20 <ugobelle@gmail.com>
2023-02-23 09:43:38 -0800

    Upper case characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _isupper - function used to pick capital letters
+ * @c: representing the alphabets variable
+ * Return: 1 and 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
