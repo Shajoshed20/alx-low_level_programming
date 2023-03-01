07ba14fceb8b1c12b7e82e66b4f5a0f0d1c187c6
Shajoshed20 <ugobelle@gmail.com>
2023-03-01 10:13:49 -0800

    concatinate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * strcat - Function to concatenate string
+ * _strcat - Function to concatenate string
  * @dest: points to concatenated string
  * @src: string variable that is the source
  *
  * Return: character type
  */
 
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int a = 0, size = 0;
 
