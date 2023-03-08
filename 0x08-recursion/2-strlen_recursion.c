3c3fb354c65e254c9fbe433f3837c2505418d254
Shajoshed20 <ugobelle@gmail.com>
2023-03-08 02:53:38 -0800

    return string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function to print length of string
+ * @s: variable for string
+ *
+ * Return: int type (string length)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
