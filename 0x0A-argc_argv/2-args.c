c95902ac709172aa22380ccafeb1c28a382f6896
Shajoshed20 <ugobelle@gmail.com>
2023-03-10 16:54:17 -0800

    prints argument on newline

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -Entry point for function that prints number of arguments passed
+ * @argc: variable for number of arguments passed when compiled
+ * @argv: variable for array of pointer to argument when compiled
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
