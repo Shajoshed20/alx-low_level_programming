1fccdc765ad15dd7758d96a6dca3b4fda9afb755
Shajoshed20 <ugobelle@gmail.com>
2023-03-10 17:13:01 -0800

    multiply two args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point for function that prints number of arguments passed
+ * @argc: variable for number of arguments passed when compiled
+ * @argv: variable for array of pointer to argument when compiled
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
