d7bbcbc42bb4e1b7dbe8bda29840d3d32a8c71f1
Shajoshed20 <ugobelle@gmail.com>
2023-03-10 17:49:27 -0800

    converts cents to coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -Entry point for function that prints the minimum number
+ * of coins to make change for an amount of money
+ * @argc: variable for number of arguments passed when compiled
+ * @argv: variable for array of pointer to argument when compiled
+ * Return: Always 0 and 1 - for none integer and 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
