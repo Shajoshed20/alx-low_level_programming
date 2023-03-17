0c0ff7cb507cd46772eb9e1a7b0440d75adef638
Shajoshed20 <ugobelle@gmail.com>
2023-03-17 15:24:36 -0700

    multiply two arguments

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -65,18 +65,14 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, num1, num2, *mul, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !int_check(s1) || !int_check(s2))
 		error();
-
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	mul = malloc(sizeof(int) * len);
-
 	if (!mul)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		mul[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
