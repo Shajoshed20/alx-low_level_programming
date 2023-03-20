7d64d3e1f5df9f6edf76aba9d0fbe6fbe5cda00c
Shajoshed20 <ugobelle@gmail.com>
2023-03-20 05:17:19 -0700

    macro to return absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - function-like macro ABS(x) that
+ *          computes the absolute value of a number x.
+ * @X: variable for value
+*/
+
+#define ABS(X) ((X < 0) ? (-X) : (X))
+
+#endif
