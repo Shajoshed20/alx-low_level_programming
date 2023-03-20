f812b64c9f75a48c34064ea3066def981d45ad09
Shajoshed20 <ugobelle@gmail.com>
2023-03-20 05:30:42 -0700

    macro for sum of 2 numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - This file contains function-like macro SUM(x, y)
+ *              that computes the sum of the numbers x and y.
+ * @x: variable for first value
+ * @y: variable for second value
+*/
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
