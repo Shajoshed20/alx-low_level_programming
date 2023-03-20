0aeb040c002f944c22cc55651277b6897d794a03
Shajoshed20 <ugobelle@gmail.com>
2023-03-20 16:02:23 -0700

    header for dog attributes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - userdefined type for dog attribute
+ * @name: variable for dog name
+ * @age: variable dog's age
+ * @owner: variable for dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
