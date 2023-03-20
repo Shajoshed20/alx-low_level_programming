65222a54aac175c7ecbd852ad7b3ad8cd002d99b
Shajoshed20 <ugobelle@gmail.com>
2023-03-20 16:10:32 -0700

    initialize dog attribute

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * init_dog - Function to initialize dog
+ * @d: pointer to dog attribute
+ * @name: variable for dog name in dog attribute
+ * @age: varaible for dog age in dog attribute
+ * @owner: variable for owner in dog attribute
+ *
+ * Return: void type
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+
+}
