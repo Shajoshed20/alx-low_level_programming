6c6943f581faac6deed9dca69b0e1f55db226db4
Shajoshed20 <ugobelle@gmail.com>
2023-03-21 06:23:19 -0700

    removes whitespace - betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	
+
 	else
 		printf("Name: %s\n", d->name);
 
@@ -27,7 +27,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	
+
 	else
 		printf("Owner: %s\n", d->owner);
 
