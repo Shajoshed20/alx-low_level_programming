b19b9b6eaf06bfed6f6335e7a95c3f51f03aa1c2
Shajoshed20 <ugobelle@gmail.com>
2023-03-23 10:35:35 -0700

    add header and readme files

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * struct Display - structure defination for display.
+ * @date_type: variable for the data type
+ * @print: variable to print function matching with data type
+ */
+typedef struct Display
+{
+	char *data_type;
+	void (*print)(va_list arg);
+
+} displays;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
