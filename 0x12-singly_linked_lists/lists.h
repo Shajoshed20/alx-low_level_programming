5aecb2326ec959c697e63c68f9cdbc855ae5aff9
Shajoshed20 <ugobelle@gmail.com>
2023-03-30 14:55:15 -0700

    added function to header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct list_s
 } list_t;
 
 /* These are the functions prototypes used for this project*/
+int _strlen(const char *len);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
