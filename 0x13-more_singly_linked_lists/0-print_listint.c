84451098b056ebf062b545d4888105eb5c96ca58
Shajoshed20 <ugobelle@gmail.com>
2023-04-03 15:06:29 -0700

    print node list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Function to print integer list
+ * @h: Variable for pointer to the head
+ *
+ * Return: value stored in the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t a;
+
+	for (a = 0; h; a++)
+	{
+		if (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+	}
+
+	return (a);
+}
