923e91fd29ab9bfdaf83f52e360988736a522f22
Shajoshed20 <ugobelle@gmail.com>
2023-04-05 14:31:09 -0700

    print list in safe mode

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - fucntion to prints a linked list
+ * @head: variable for pointer to head node
+ *
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	const listint_t *tortoise = head, *hare = head;
+
+	if (head)
+	{
+		while (tortoise && hare && hare->next)
+		{
+			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+			tortoise = tortoise->next;
+			hare = hare->next->next;
+
+			if (tortoise == hare)
+			{
+				printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+				exit(98);
+			}
+			i++;
+		}
+
+		if (tortoise == hare)
+		{
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			exit(98);
+		}
+
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			i++;
+		}
+
+		return (i);
+	}
+}
