5680f857e7fff26ac900da4de88ebc43242b022c
Shajoshed20 <ugobelle@gmail.com>
2023-04-08 10:27:18 -0700

    file changed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,26 +4,6 @@
 void cpy_file(const char *src_path, const char *dest_path);
 
 /**
-  * main - Entry point
-  * @argc: variable for the number of argument
-  * @argv: variable to pointer for argument vector
-  *
-  * Return: Always (0) Success
-  */
-int main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	cpy_file(argv[1], argv[2]);
-
-	exit(0);
-}
-
-/**
  * cpy_file - Function to copy content from one file to the other
  * @src_path: variable for the source path of file
  * @dest_path: variable for the destination path of file
@@ -36,11 +16,17 @@ void cpy_file(const char *src_path, const char *dest_path)
 	char buffer[BUF_SIZE];
 
 	src_file = open(src_path, O_RDONLY);
-	dest_file = open(dest_path, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (!src_file || dest_file == -1)
+	if (!src_path || src_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_path);
 		exit(98);
+	}
+
+	dest_file = open(dest_path, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_path);
+		exit(99);
 	}
 
 	while ((read_size = read(src_file, buffer, BUF_SIZE)) > 0)
@@ -71,4 +57,24 @@ void cpy_file(const char *src_path, const char *dest_path)
 		exit(100);
 	}
 
+}
+
+/**
+  * main - Entry point
+  * @argc: variable for the number of argument
+  * @argv: variable to pointer for argument vector
+  *
+  * Return: Always (0) Success
+  */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cpy_file(argv[1], argv[2]);
+	
+	exit(0);
 }
