76e0dc684d6ab7145a02ed95fbc836459cc5bc8b
Shajoshed20 <ugobelle@gmail.com>
2023-04-10 15:50:18 -0700

    betty correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,26 @@
 void cpy_file(const char *src_path, const char *dest_path);
 
 /**
+  * main - Entry point
+  * @argc: variable for the number of argument
+  * @argv: variable to pointer for argument vector
+  *
+  * Return: Always (0) Success
+  */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cpy_file(argv[1], argv[2]);
+
+	exit(0);
+}
+
+/**
  * cpy_file - Function to copy content from one file to the other
  * @src_path: variable for the source path of file
  * @dest_path: variable for the destination path of file
@@ -13,7 +33,7 @@ void cpy_file(const char *src_path, const char *dest_path);
 void cpy_file(const char *src_path, const char *dest_path)
 {
 	int src_file, dest_file, read_size, write_size;
-	char buffer[1024];
+	char buffer[BUF_SIZE];
 
 	src_file = open(src_path, O_RDONLY);
 	if (!src_path || src_file == -1)
@@ -24,7 +44,7 @@ void cpy_file(const char *src_path, const char *dest_path)
 
 	dest_file = open(dest_path, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	while ((read_size = read(src_file, buffer, 1024)) > 0)
+	while ((read_size = read(src_file, buffer, BUF_SIZE)) > 0)
 	{
 		write_size = write(dest_file, buffer, read_size);
 		if (write_size != read_size || dest_file == -1)
@@ -52,24 +72,4 @@ void cpy_file(const char *src_path, const char *dest_path)
 		exit(100);
 	}
 
-}
-
-/**
-  * main - Entry point
-  * @argc: variable for the number of argument
-  * @argv: variable to pointer for argument vector
-  *
-  * Return: Always (0) Success
-  */
-int main(int argc, char **argv)
-{
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	cpy_file(argv[1], argv[2]);
-	
-	exit(0);
 }
